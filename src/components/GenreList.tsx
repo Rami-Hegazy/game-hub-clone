7eb56a236858626f12a1ea85975b85703e8d340d
Rami Hegazy <ramihegaz90@gmail.com>
2024-09-08 10:16:57 +0300

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,15 @@
+import useGenere from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres } = useGenere();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Genre{
+    id: number;
+    name: string;
+}
+
+interface FetchGenreResponse {
+    count: number;
+    results: Genre[];
+}
+const useGenere = () => {
+    const [genres, setGenre] = useState<Genre[]>([]);
+    const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    const contoller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGenreResponse>("/genres", { signal: contoller.signal })
+      .then((res) => {
+        setGenre(res.data.results)
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+        setLoading(false);});
+    return () => contoller.abort(); 
+  } , []);
+  return { genres, error, isLoading };
+}
+
+export default useGenere
